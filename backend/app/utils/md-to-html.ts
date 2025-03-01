3bc3e0c14ea977938dbf74994d7cd74b87b5033f
Aaron <aaronklinker1@gmail.com>
2025-03-01 11:44:48 -0600

    fix: Links should open in parent window

--- backend/app/utils/md-to-html.ts
+++ backend/app/utils/md-to-html.ts
@@ -1,6 +1,30 @@
-import { Converter } from "showdown";
+import { Converter, type ShowdownExtension } from "showdown";
+import { isIframe } from "./is-iframe";
 
-const converter = new Converter();
+const converter = new Converter({
+  extensions: [linkBaseUrl(), linkParentTarget()],
+});
+
 export function mdToHtml(md: string): string {
   return converter.makeHtml(md);
+}
+
+function linkBaseUrl(): ShowdownExtension {
+  return {
+    type: "output",
+    regex: /href="\//,
+    replace: `href="${DOCS_URL}/`,
+  };
+}
+
+function linkParentTarget(): ShowdownExtension[] {
+  if (!isIframe) return [];
+
+  return [
+    {
+      type: "output",
+      regex: /href="\//,
+      replace: `href="${DOCS_URL}/`,
+    },
+  ];
 }
